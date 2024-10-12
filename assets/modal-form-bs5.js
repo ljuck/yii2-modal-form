141344345ee5aa88d7272fc8cf03d955eaefb4dc
ljuck <80886983+ljuck@users.noreply.github.com>
2024-10-12 15:28:24 +0200

    Create  modal-form-bs5.js

---
+++ assets/modal-form-bs5.js
@@ -0,0 +1,124 @@
+/**
+ * @link https://github.com/borodulin/yii2-modal-form
+ * @copyright Copyright (c) 2016 Andrey Borodulin
+ * @license https://github.com/borodulin/yii2-modal-form/blob/master/LICENSE
+ */
+;
+(function($) {
+
+    var idCounter = 0;
+
+    var self = this;
+    
+    $.createModalForm = function(options, clientOptions) {
+        options = $.extend({
+            singleton: true,
+            size : null,
+            loginUrl : null,
+            loading : '<div class="modal-header">Loading...</div>'
+                    + '<div class="modal-body">'
+                    + '<div class="progress progress-striped active" style="margin-bottom:0;">'
+                    + '<div class="progress-bar" style="width: 100%"></div>'
+                    + '</div>' + '</div>',
+            modal : '<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">'
+                    + '<div class="modal-dialog">'
+                    + '<div class="modal-content"></div>' + '</div>' + '</div>',
+            error : '<div class="error-summary"></div>'
+        }, options);
+        if (options.singleton && self.hasOwnProperty('modalDialog') && self.modalDialog) {
+            if ('size' in options) {
+                self.modalDialog.find('.modal-dialog').removeClass('modal-sm modal-lg').addClass(options.size);
+            }
+            return self.modalDialog;
+        }
+        var $modalDialog = $(options.modal).attr('id', 'modalFormId-' + ++idCounter);
+        if (options.singleton) {
+            self.modalDialog = $modalDialog;    
+        }
+        if (options.size) {
+            $modalDialog.find('.modal-dialog').addClass(options.size);
+        }
+        if (clientOptions) {
+            if (clientOptions.id) {
+                $modalDialog.attr('id', clientOptions.id);
+            }
+            if (clientOptions.class) {
+                $modalDialog.addClass(clientOptions.class);
+            }
+            if (clientOptions.tabindex) {
+                $modalDialog.attr('tabindex', clientOptions.tabindex);
+            } else if (clientOptions.tabindex == false) {
+                $modalDialog.removeAttr('tabindex');
+            }
+            if (clientOptions.backdrop) {
+                $modalDialog.attr('data-bs-backdrop', clientOptions.backdrop);
+            } else if (clientOptions.backdrop == false) {
+                $modalDialog.removeAttr('data-bs-backdrop');
+            }
+            if (clientOptions.backdrop) {
+                $modalDialog.attr('data-bs-keyboard', clientOptions.keyboard);
+            } else if (clientOptions.keyboard == false) {
+                $modalDialog.removeAttr('data-bs-keyboard');
+            }
+        }
+        $('body').append($modalDialog);
+        var $content = $modalDialog.find('.modal-content');
+        $modalDialog.loading = function() {
+            return $content.html(options.loading);
+        };
+        $modalDialog.error = function($error) {
+            return $content.html($(options.error).html($error));
+        };
+        $modalDialog.content = function(html) {
+            return $content.html(html);
+        };
+        $modalDialog.on('hidden.bs.modal', function() {
+            $(this).remove();
+            if (options.singleton) {
+                self.modalDialog = null;
+            }
+        });
+        $modalDialog.ajaxContent = function(ajax) {
+            $modalDialog.modal('show').loading();
+            $.ajax($.extend(ajax, {
+                success : function(response) {
+                    $modalDialog.content(response);
+                    if ('success' in options) {
+                        eval(options.success);
+                    }
+                },
+                error : function(jqXHR) {
+                    if ((jqXHR.status === 401) && (options.loginUrl)) {
+                        $modalDialog.ajaxContent({
+                            url : options.loginUrl
+                        });
+                    } else {
+                        $modalDialog.error(jqXHR.responseText);
+                    }
+                }
+            }));
+        };
+        
+        $modalDialog.on('submit', 'form', function(event) {
+            event.preventDefault();
+            var form = this;
+            var $form = $(form);
+            var method = $form.attr('method').toLowerCase() || 'post';
+            var ajax = {
+                url : $form.attr('action')
+            };
+            if (method === 'post') {
+                ajax.data = new FormData(form);
+                ajax.type = 'post';
+                ajax.enctype = 'multipart/form-data';
+                ajax.processData = false; // tell jQuery not to process the data
+                ajax.contentType = false; // tell jQuery not to set contentType
+            } else {
+                ajax.data = $form.serialize();
+            }
+            $modalDialog.ajaxContent(ajax);
+            return false;
+        });
+        return $modalDialog;
+    };
+})(window.jQuery);
